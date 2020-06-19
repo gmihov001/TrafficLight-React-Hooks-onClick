3f425247975f9bdc7fabe21022c273a824ba669c
George Mihov <gmihov001@gmail.com>
2020-06-19 19:00:44 +0000

    Updates

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -4,15 +4,15 @@ import React from "react";
 export function Home() {
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
+			<h1>Traffic Light</h1>
+			<div className="traffic-light">
+				<div className="red">RED</div>
+				<div className="yellow">Yellow</div>
+				<div className="green">Green</div>
+			</div>
 			<button className="btn btn-success">
 				If you see this green button... bootstrap is working
 			</button>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
 		</div>
 	);
 }
