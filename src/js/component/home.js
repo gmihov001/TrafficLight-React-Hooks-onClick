ef1609f1bad6589dec159242e293215d4b9f48a9
George Mihov <gmihov001@gmail.com>
2020-06-19 19:22:16 +0000

    Updates

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,14 +2,22 @@ import React, { useState } from "react";
 
 //create your first component
 export function Home() {
-	const [color, setColor] = useState("red");
+	const [color, setColor] = useState(null);
 	return (
 		<div className="text-center mt-5">
 			<h1>Traffic Light</h1>
 			<div className="traffic-light">
-				<div className="red">RED</div>
-				<div className="yellow">Yellow</div>
-				<div className="green">Green</div>
+				<div
+					className={color === "red" ? "red active" : "red inactive"}
+					onClick={() => setColor("red")}>
+					Red
+				</div>
+				<div className="yellow" onClick={() => setColor("yellow")}>
+					Yellow
+				</div>
+				<div className="green" onClick={() => setColor("green")}>
+					Green
+				</div>
 			</div>
 			<button className="btn btn-success">
 				If you see this green button... bootstrap is working
