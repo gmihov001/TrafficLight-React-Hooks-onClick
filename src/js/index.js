a329e960473e3e3bde6668a54ec2aea9360928fa
George Mihov <gmihov001@gmail.com>
2020-10-07 16:11:34 +0000

    Updates

--- src/js/index.js
+++ src/js/index.js
@@ -12,21 +12,19 @@ import "../styles/index.scss";
 import { Home } from "./component/home.js";
 
 //render your react application
-var prevColor = "red";
+var prevColor = "yellow";
 
 const runLight = () => {
 	if (prevColor == "red") {
 		ReactDOM.render(<Home color="green" />, document.querySelector("#app"));
 		prevColor = "green";
-	}
-	if (prevColor == "green") {
+	} else if (prevColor == "green") {
 		ReactDOM.render(
 			<Home color="yellow" />,
 			document.querySelector("#app")
 		);
 		prevColor = "yellow";
-	}
-	if (prevColor == "yellow") {
+	} else if (prevColor == "yellow") {
 		ReactDOM.render(<Home color="red" />, document.querySelector("#app"));
 		prevColor = "red";
 	}
