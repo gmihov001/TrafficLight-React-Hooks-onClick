6d6cdfc0e9cb9847969a4a3540d2b72281f27055
George Mihov <gmihov001@gmail.com>
2021-01-11 20:05:45 +0000

    Updates

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -9,24 +9,18 @@ export function Home(props) {
 			<h1>Traffic Light</h1>
 			<div className="traffic-light">
 				<div
-					className={
-						color === "red" ? "red active" : "red inactive"
-					}
+					className={color === "red" ? "red active" : "red inactive"}
 					onClick={() => setColor("red")}
 				/>
 				<div
 					className={
-						color === "yellow"
-							? "yellow active"
-							: "yellow inactive"
+						color === "yellow" ? "yellow active" : "yellow inactive"
 					}
 					onClick={() => setColor("yellow")}
 				/>
 				<div
 					className={
-						color === "green"
-							? "green active"
-							: "green inactive"
+						color === "green" ? "green active" : "green inactive"
 					}
 					onClick={() => setColor("green")}
 				/>
